a59e4e533a27304773d98238d9f8a52a75d14beb
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-29 19:47:42 -0300

    implementado primeiro objeto, uma esfera

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -9,15 +11,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ArCoreController arCoreController;
+
+  _onArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+    _addSphere(arCoreController);
+  }
+
+  _addSphere(ArCoreController _arCoreController) {
+    final material = ArCoreMaterial(color: Colors.deepOrange);
+    final sphere = ArCoreSphere(materials: [material], radius: 0.2);
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(0, 0, -1),
+    );
+
+    _arCoreController.addArCoreNode(node);
+  }
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ArCoreView(
+          onArCoreViewCreated: _onArCoreViewCreated,
+        ));
   }
 }
